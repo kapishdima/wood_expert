981ef7e733192e92ebb96666c4fa0dbd091ffb90
DimaDevelopment <dima.development@fakel.com.ua>
2022-12-16 19:21:43 +0200

    added chatsIds to env file

--- src/js/telegram/telegram.js
+++ src/js/telegram/telegram.js
@@ -2,14 +2,20 @@ const TELEGRAM_API_KEY = process.env.TELEGRAM_API_KEY;
 const TELEGRAM_API_URL = process.env.TELEGRAM_API_URL;
 
 export const sendMessage = async (message, reply) => {
-  const chatId = 593128405;
-  let url = `${TELEGRAM_API_URL}${TELEGRAM_API_KEY}/sendMessage?chat_id=${chatId}&text=${message}&parse_mode=markdown`;
-
-  if (reply) {
-    url += `&reply_markup=${JSON.stringify(reply)}`;
+  const chats = JSON.parse(process.env.TELEGRAM_CHATS);
+  if (!chats || !chats.length) {
+    return;
   }
 
-  const response = await fetch(url, { method: 'GET' });
+  chats.forEach(async (chat) => {
+    let url = `${TELEGRAM_API_URL}${TELEGRAM_API_KEY}/sendMessage?chat_id=${chat}&text=${message}&parse_mode=markdown`;
 
-  return response.json();
+    if (reply) {
+      url += `&reply_markup=${JSON.stringify(reply)}`;
+    }
+
+    const response = await fetch(url, { method: 'GET' });
+
+    return response.json();
+  });
 };
